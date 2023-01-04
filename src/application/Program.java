c2a0f441490b4a1cd16edb7c21883c52b2a455a0
Kelvin Marcondes <kelvindesouza@hotmail.com>
2023-01-04 00:08:47 -0300

    Class position, Created class: Board and Piece, Camada Chess e imprimindo tabuleiro

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import chess.ChessMatch;
+
+import static application.UI.printBoard;
+
+public class Program {
+    public static void main(String[] args) {
+
+        ChessMatch chessMatch = new ChessMatch();
+        printBoard(chessMatch.getPieces());
+    }
+}
